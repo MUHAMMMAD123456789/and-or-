63b918b5f92da08c523824e195281a65fea22392
Ali <daminovmuhammadali3@gmail.com>
2024-02-16 18:12:46 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+// && --- and
+// || --- or
+
+
+
+// let name = prompt("Ваше имя?").toLowerCase().trim()
+
+// if( name[0] === "a" && name.length % 2 == 0) {
+//     alert("Welcome")
+// } else {
+//     alert("go gome")
+// }
+
+
+
+
+// let guest_list = "putin biden zelenskiy mirziyoev tokaev"
+
+// let name = prompt("What is ur name").toLowerCase().trim()
+
+
+// if( guest_list.includes(name) ){
+//     alert("welcome")
+// }  else (
+//     alert("go gome")
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+let name = "alex"
+let password = 7777
+let money = 10000 
+
+
+let ask = prompt('Как вас зовут?').toLowerCase().trim()
+if (ask === name){
+    
+}   else {
+           alert('Нет')
+         }
+
+
+let ask_password = +prompt('Какой ваш код?')
+    if(ask_password === password){
+       
+    }  else  {
+        alert('Нет')
+    }
+
+
+
+let ask_money = prompt('Сколько снять')
+    if (ask_money < money){
+            alert(`Осталось  ${money - ask_money}`)
+        }  else  {
+            alert('У вас мало денег')
+        }
+
+
+
+// let capacity = prompt("Сколько человек?")
+// if (capacity <= 10){
+//     alert('Отлично все')
+// } else {
+//     alert('Нельзя брат')
+// }
+            
+             
